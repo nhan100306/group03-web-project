6b1b3e8a632d60a452fdf0e9a10b47f48ccd8611
nhan100306 <nguyenthanhjhan.1999@gmail.com>
2026-06-25 14:23:39 +0700

    hi

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Kiểm tra xem file JavaScript đã kết nối thành công chưa
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Chúc mừng! Cấu trúc source code ban đầu đã được tải thành công.");
+});
